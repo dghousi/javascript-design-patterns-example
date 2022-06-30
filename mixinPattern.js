ff36c6c101ca93128bca5105d5d0f7a7d47dd52f
Dawlatzai <dawlatzai.ghousi@gmail.com>
2022-06-30 08:27:33 +0430

    Add mixin pattern

---
+++ mixinPattern.js
@@ -0,0 +1,24 @@
+// mixin
+let sayHiMixin = {
+  sayHi() {
+    alert(`Hello ${this.name}`)
+  },
+  sayBye() {
+    alert(`Bye ${this.name}`)
+  },
+}
+
+// usage:
+class User {
+  constructor(name) {
+    this.name = name
+  }
+}
+
+console.log(User.prototype)
+// copy the methods
+Object.assign(User.prototype, sayHiMixin)
+
+// now User can say hi
+new User('Dude').sayHi() // Hello Dude!
+new User('Dude').sayBye()
